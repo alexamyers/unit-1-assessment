db80b0cabe84472adaea2e353f252418ca467465
Alex Myers <alex.myers919@gmail.com>
2022-02-26 07:58:38 -0500

    first commit

---
+++ gitInfo.js
@@ -0,0 +1,78 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+const gitDefinition = "Git is software made for tracking changes in files, which helps to coordinate workflows collaboratively.";
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+const gitHubDefinition = "Github is a internet hosting provider for software development and version control via Git.";
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+const gitInitDefinition = "The command `git init` creates a new Git repository, ie initializes the repository.";
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+const gitCloneDefinition = "The command `git clone` essentially copies an existing repository for use in another location or directory.";
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+const gitStatusDefinition = "`git status` displays the current state of the working directory and allows one to see what changes have been staged, which haven't and which files are being tracked.";
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+const gitAddDefinition = "`git add` adds a change to the working directory and places it in the staging area.";
+const gitAddCode = "git add .";
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+const gitCommitDefinition = "`git commit` takes a snapshot of the repo's currently staged changes.";
+const gitCommitCode = "git commit -m 'initial commit'.";
+
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+const gitPushDefinition = "`git push` is used to upload a local repo to a remote repo.";
---
+++ password.js
@@ -0,0 +1,5 @@
+console.log("Welcome to the password validator tool.");
+
+
+let password = prompt("What would you like your password to be?");
+
